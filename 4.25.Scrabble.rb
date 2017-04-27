4dc3177ead9c2150bac467c7b76b75d0f6a7703f
Michael Yatco <mjyatco@gmail.com>
2017-04-27 16:00:34 -0400

    Solved with reduce method

--- 4.25.Scrabble.rb
+++ 4.25.Scrabble.rb
@@ -3,13 +3,14 @@
 #Use the `reduce` method to perform the computation in your scrabble_score method. The code should work as follows:
 #p scrabble_score("scholar")  #=> 12
 
-def scrabble_score(word)
-  letters = word.split(//)
+def scrabble_score(string)
   points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 'y'=> 4, 'z'=> 10}
-  
- points.values.reduce(:+)
+  p string #"hi"
+  p string.chars #["h","i"]
+  p string.chars.map ##<Enumerator:["h","i"]:map 
+  p string.chars.map { |letter| points[letter]} #[4,1]
 
-
+  string.chars.map { |letter| points[letter.downcase] }.reduce(:+)
 end
 
 p scrabble_score("hi")

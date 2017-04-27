8468f20b492b93c952bb5abb0ee2a5b84687783e
Michael Yatco <mjyatco@gmail.com>
2017-04-27 16:15:52 -0400

    Second answer w/o reduce method

--- 4.25.Scrabble.rb
+++ 4.25.Scrabble.rb
@@ -14,3 +14,23 @@ def scrabble_score(string)
 end
 
 p scrabble_score("hi")
+
+#Without the reduce method
+def scrabble_score(word)
+  points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 'y'=> 4, 'z'=> 10}
+   x = 0
+   score = 0
+  
+     while x < word.length
+       points.each do |letter, point|
+         if letter == word[x]
+         score += point
+         end
+       end
+     x += 1
+     end
+ 
+ return score
+ 
+end
+puts scrabble_score("scholar")

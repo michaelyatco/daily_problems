431fad12a996dd0a0baec2492b683d37e6a67d8a
Michael Yatco <mjyatco@gmail.com>
2017-04-29 16:56:24 -0400

    L33tspeak method

---
+++ 4.27.L33tspeak.rb
@@ -0,0 +1,23 @@
+#L33tspeak - the act of speaking like a computer hacker (or hax0r) - was popularized in the late 1990s as a mechanism of abusing ASCII art and character mappings to confuse outsiders. It was a lot of fun. One popular comic strip in 2000 showed just how far the joke ran.
+#In L33Tspeak you substitute letters for their rough outlines in ASCII characters, e.g. symbols or numbers. You can have 1:1 mappings (like E -> 3) or 1:many mappings (like W -> `//). So then you wind up with words like this:
+#BASIC => 6451C
+#ELEET => 31337 (pronounced elite)
+#WOW => `//0`//
+#MOM => (V)0(V)
+
+#Mappings
+
+#For this challenge we'll be using a subset of American Standard Leetspeak:
+#A -> 4
+#B -> 6
+#E -> 3
+#I -> 1
+#L -> 1
+#M -> (V)
+#N -> (\)
+#O -> 0
+#S -> 5
+#T -> 7
+#V -> \/
+#W -> `//
+#Your challenge, should you choose to accept it, is to translate to and from L33T.

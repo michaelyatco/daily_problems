081810de4819a64cfc27ffd8f9ed0ee5f3da7668
michaelyatco <mjyatco@gmail.com>
2017-06-14 14:50:09 -0400

    Recursion

--- 6.14.Functions.js
+++ 6.14.Functions.js
@@ -76,3 +76,88 @@ function multiplier(factor) {
 var twice = multiplier(2);
 console.log(twice(5));
 // → 10
+
+//Recursion
+function power(base, exponent) {
+  if (exponent == 0)
+    return 1;
+  else
+    return base * power(base, exponent - 1);
+}
+
+console.log(power(2, 3));
+// → 8
+// Recursion vs Loop - Elegant vs Efficient
+
+function findSolution(target) {
+  function find(start, history) {
+    if (start == target)
+      return history;
+    else if (start > target)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24));
+// → (((1 * 3) + 5) * 3)
+
+// //Growing Functions
+function printFarmInventory(cows, chickens) {
+  var cowString = String(cows);
+  while (cowString.length < 3)
+    cowString = "0" + cowString;
+  console.log(cowString + " Cows");
+  var chickenString = String(chickens);
+  while (chickenString.length < 3)
+    chickenString = "0" + chickenString;
+  console.log(chickenString + " Chickens");
+}
+printFarmInventory(7, 11);
+
+//Exercise Minimum
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+  	return b;
+}
+
+ console.log(min(0,10));
+ console.log(min(0,-10));
+
+//Exerceise Recursion
+function isEven(n) {
+  if (n == 0)
+    return true;
+  else if (n == 1)
+  	return false;
+  else if (n < 0)
+  	return isEven(-n);
+  else
+  	return isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+//Exercise Bean Counting
+
+function countChar(string, ch) {
+  var counted = 0
+  for (var i = 0; i < string.length; i++)
+    if (string.charAt(i) == ch)
+    	counted += 1;
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("Boston Bruins"));
+console.log(countChar("Mississippi", "s"))

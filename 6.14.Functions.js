76de5d36d834584313db97ce62f43dd95e485779
michaelyatco <mjyatco@gmail.com>
2017-06-14 09:48:49 -0400

    Functions.js

---
+++ 6.14.Functions.js
@@ -0,0 +1,78 @@
+// Function
+var power = function(base, exponent) {
+  var result = 1;
+  for (var count = 0; count < exponent; count++)
+    result *= base;
+  return result;
+};
+
+console.log(power(2, 10));
+
+// Parameters and Scope
+var x = "outside";
+
+var f1 = function() {
+  var x = "inside f1";
+};
+f1();
+console.log(x);
+// → outside
+
+var f2 = function() {
+  x = "inside f2";
+};
+f2();
+console.log(x);
+// → inside f2
+
+// Functions within Functionsvar landscape = function() {
+var landscape = function() {
+  var result = "";
+  var flat = function(size) {
+    for (var count = 0; count < size; count++)
+      result += "_";
+  };
+  var mountain = function(size) {
+    result += "/";
+    for (var count = 0; count < size; count++)
+      result += "'";
+    result += "\\";
+  };
+
+  flat(3);
+  mountain(4);
+  flat(6);
+  mountain(1);
+  flat(1);
+  return result;
+};
+
+console.log(landscape());
+// → ___/''''\______/'\_
+
+// Call Stack
+function greet(who) {
+  console.log("Hello " + who);
+}
+greet("Harry");
+console.log("Bye");
+
+// function chicken() {
+//   return egg();
+// }
+// function egg() {
+//   return chicken();
+// }
+// console.log(chicken() + " came first.");
+// This will break
+
+// Closure
+function multiplier(factor) {
+  return function(number) {
+    return number * factor;
+  };
+}
+
+var twice = multiplier(2);
+console.log(twice(5));
+// → 10
